250f693d898fca0102e0ee1010baab7e5e9c5fe5
BuildTools <unconfigured@null.spigotmc.org>
2023-09-28 11:31:57 +0200

    [add] Add expr comparison

--- app/Main.hs
+++ app/Main.hs
@@ -5,3 +5,4 @@ main :: IO ()
 main = do
     print $ evalExpr (List [Symbol "+", Number 10, List[Symbol "/", Number 6, Number 2]])
     print $ evalExpr (List [Symbol "eq?", Number 10, Number 10])
+    print $ evalExpr (List [Symbol ">", Number 8, Number 10])
--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -28,6 +28,14 @@ apply env (Symbol s) args = case s of
                    then Symbol "true" 
                    else Symbol "false"
            else error "'eq?' expects exactly two arguments"
+    "<" ->
+        if length args == 2 && compareExprs env (head args) (args !! 1) == Just LT
+           then Symbol "true"
+           else Symbol "false"
+    ">" ->
+        if length args == 2 && compareExprs env (head args) (args !! 1) == Just GT
+           then Symbol "true"
+           else Symbol "false"
     _   -> error $ "Unknown function: " ++ s
 apply _ _ _ = error "Expected symbol at head of list"
 
@@ -48,6 +56,15 @@ equalExpr env (Number n1) (Number n2) = n1 == n2
 equalExpr env (Symbol s1) (Symbol s2) = s1 == s2
 equalExpr env (List l1) (List l2) = all (uncurry (equalExpr env)) (zip l1 l2)
 equalExpr _ _ _ = False
+
+compareExprs :: Env -> Expr -> Expr -> Maybe Ordering
+compareExprs env (Number n1) (Number n2) = Just $ compare n1 n2
+compareExprs env (Symbol s1) (Symbol s2) = Just $ compare s1 s2
+compareExprs env (List l1) (List l2) = foldl combine (Just EQ) (zipWith (compareExprs env) l1 l2)
+  where
+    combine (Just EQ) o = o
+    combine _ _ = Nothing
+compareExprs _ _ _ = Nothing
 
 evalExpr :: Expr -> Expr
 evalExpr = eval initialEnv
