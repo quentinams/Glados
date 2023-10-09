fe4d563c0fdc7da085a998720337a03c6673e966
Matheo <matheo.beaunez@epitech.eu>
2023-10-09 15:56:33 +0200

    add parser function

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,211 @@ import Control.Exception
 import Datas
 import Env
 import Evaluation
-import Control.Monad (foldM)
+-- import Control.Monad (foldM)
+
+import Control.Applicative (Alternative(..))
+-- import Control.Applicative ((<*>))
+import Control.Monad (void)
+
+-- data Expr = Symbol String | Number Float | Bool Bool | List [Expr] | Lambda [String] Expr | Func [String] Expr deriving (Eq)
+
+-- instance Show Expr where
+--     show (Symbol s) = show s
+--     show (Number n) = if fromIntegral (floor n) == n then show (floor n) else show n
+--     show (Bool b) = if b then "#t" else "#f"
+--     show (List l) = show l
+--     show (Lambda params body) = "#<procedure>"
+--     show (Func params body) = "#<procedure>"
+
+data Parser a = Parser {
+    runParser :: String -> Maybe (a, String)
+}
+
+-- <$> = fmap map d'un parseur à la valeur qui est dans le parseur
+-- Symbol <$> parseSome utilise fmap pour construire un Symbol à partir du résultat de parseSome.
+instance Functor Parser where
+    fmap f p = Parser $ \s -> case runParser p s of
+        Just (a, rest) -> Just (f a, rest)
+        Nothing -> Nothing
+
+
+-- <*> combine deux parseurs 1: produit une fonction 2: produit une valeur
+-- pure place une valeur dans un contexte minimal
+instance Applicative Parser where
+    pure a = Parser $ \input -> Just (a, input)
+
+    p1 <*> p2 = Parser $ \input ->
+        case runParser p1 input of
+            Nothing -> Nothing
+            Just (f, remaining1) ->
+                case runParser p2 remaining1 of
+                    Nothing -> Nothing
+                    Just (a, remaining2) -> Just (f a, remaining2)
+
+--tente d'abord le premier parseur si il echoue fait le deuxieme  <|>
+-- empty parseur qui echoue toujours
+instance Alternative Parser where
+    empty = Parser $ \_ -> Nothing
+    p1 <|> p2 = Parser $ \input ->
+        case runParser p1 input of
+            Just _  -> runParser p1 input
+            Nothing -> runParser p2 input
+
+-- >>= est utilisé pour séquencer deux actions, où la sortie de la première action détermine la seconde action à exécuter.
+
+instance Monad Parser where
+    return = pure
+    p >>= f = Parser $ \input ->
+        case runParser p input of
+            Nothing -> Nothing
+            Just (a, remaining) -> runParser (f a) remaining
+
+instance MonadFail Parser where
+    fail msg = Parser $ \_ -> error msg
+
+parseChar :: Char -> Parser Char
+parseChar c = Parser $ \s -> case s of
+    (x:xs) -> if x == c then Just (c, xs) else Nothing
+    [] -> Nothing
+
+parseAnyChar :: String -> Parser Char
+parseAnyChar chars = Parser $ \input -> case input of
+    (x:xs) | x `elem` chars -> Just (x, xs)
+    _ -> Nothing
+
+parseAnd :: Parser a -> Parser b -> Parser (a, b)
+parseAnd p1 p2 = Parser $ \input ->
+    case runParser p1 input of
+        Nothing -> Nothing
+        Just (result1, remaining1) ->
+            case runParser p2 remaining1 of
+                Nothing -> Nothing
+                Just (result2, remaining2) -> Just ((result1, result2), remaining2)
+
+parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+parseAndWith f p1 p2 = Parser $ \input ->
+    case runParser (parseAnd p1 p2) input of
+        Nothing -> Nothing
+        Just ((result1, result2), remaining) -> Just (f result1 result2, remaining)
+
+
+parseMany :: Parser a -> Parser [a]
+parseMany p = Parser $ \input ->
+    case runParser p input of
+        Nothing -> Just ([], input)
+        Just (result, remaining) ->
+            case runParser (parseMany p) remaining of
+                Nothing -> Just ([result], remaining)
+                Just (results, finalRemaining) -> Just (result:results, finalRemaining)
+
+parseSome :: Parser a -> Parser [a]
+parseSome p =
+    parseAndWith (\x xs -> x:xs) p (parseMany p)
+
+
+-- parseManyCount :: Parser a -> Parser Int
+-- parseManyCount p = length <$> parseMany p -- <$> est un alias de fmap 1- analyse lance parsemany 2- lance length sur le resultat de parsemany
+
+-- parseSomeCount :: Parser a -> Parser Int
+-- parseSomeCount p = length <$> parseSome p
+
+parseSymbol :: Parser Expr
+parseSymbol = do
+    skipSpaces
+    Symbol <$> parseSome (parseAnyChar (['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9' ] ++ "-_"))
+
+parseNumber :: Parser Expr
+parseNumber = do
+    skipSpaces
+    numStr <- parseSome (parseAnyChar ['0'..'9'])
+    return $ Number (read numStr :: Float)
+
+
+parseBool :: Parser Expr
+parseBool = do
+    skipSpaces
+    (Bool True <$ (parseChar '#' *> parseChar 't'))
+    <|> (Bool False <$ (parseChar '#' *> parseChar 'f'))
+
+parseList :: Parser Expr
+parseList = do
+    skipSpaces
+    _ <- parseChar '('
+    exprs <- parseMany (skipSpaces *> parseExpr <* skipSpaces)
+    _ <- parseChar ')'
+    return $ List exprs
+
+
+parseLambda :: Parser Expr
+parseLambda = do
+    skipSpaces
+    _ <- parseChar '('
+    _ <- parseChar '\\'
+    _ <- parseChar '('
+    params <- parseMany (do
+        skipSpaces
+        Symbol sym <- parseSymbol
+        return sym)
+    _ <- parseChar ')'
+    skipSpaces
+    body <- parseExpr
+    _ <- parseChar ')'
+    return (Lambda params body)
+
+parseFunc :: Parser Expr
+parseFunc = do
+    skipSpaces
+    _ <- parseString "Function"
+    skipSpaces
+    _ <- parseChar '('
+    params <- parseMany (do
+        skipSpaces
+        Symbol sym <- parseSymbol
+        return sym)
+    _ <- parseChar ')'
+    skipSpaces
+    body <- parseExpr
+    return (Func params body)
+
+-- Un parseur qui reconnaît n'importe quel type d'Expr
+parseExpr :: Parser Expr
+parseExpr = parseNumber
+        <|> parseSymbol
+        <|> parseBool
+        <|> parseList
+        <|> parseLambda
+        <|> parseFunc
+
+
+-- Utilitaire pour parser une chaîne exacte
+parseString :: String -> Parser String
+parseString = traverse (parseAnyChar . pure)
+
+skipSpaces :: Parser ()
+skipSpaces = void $ parseMany (parseAnyChar " \t\n\r")
+
+
 
 main :: IO ()
 main = do
-    let expressions = 
-            [ List [Symbol "define", Symbol "x", Number 5], List [Symbol "+", Symbol "x", Number 10]]
+    loop
+  where
+    loop = do
+        putStrLn "Enter your lisp expression (or type 'quit' to exit):"
+        input <- getLine -- Obtenir une entrée de l'utilisateur
+        if input == "quit"
+            then putStrLn "Goodbye!"
+            else do
+                case runParser parseExpr input of
+                    Just (expr, _) -> do
+                        (finalEnv, maybeLastResult) <- evalAndStore (initialEnv, Nothing) expr
+                        case maybeLastResult of
+                            Just lastResult -> putStrLn $ show lastResult
+                            Nothing -> putStrLn "No result to show."
+                    Nothing -> putStrLn "Failed to parse the lisp expression."
+                loop
 
-    (finalEnv, maybeLastResult) <- foldM evalAndStore (initialEnv, Nothing) expressions
-    case maybeLastResult of
-        Just lastResult -> putStrLn $ show lastResult
-        Nothing -> putStrLn "No result to show."
-    return ()
+
 
 evalAndStore :: (Env, Maybe Expr) -> Expr -> IO (Env, Maybe Expr)
 evalAndStore (env, _) expr = do
